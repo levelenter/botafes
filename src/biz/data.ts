a99fedc398857dab7c36892a97a6e1802af57566
山本大 <dai@yamamotodainoMacBook-Pro.local>
2022-11-04 15:06:42 +0900

    data

--- src/biz/data.ts
+++ src/biz/data.ts
@@ -12,21 +12,48 @@ export type TreasureData = {
 export const data: TreasureData[] = [
   {
     id: 1,
-    lat: 34.6745469127447,
-    lng: 135.49032039571802,
-    title: "堀江小学校",
+    lat: 34.844116,
+    lng: 135.4297586,
+    title: "p1",
     el: "treasure1",
     checked: false,
   },
   {
     id: 2,
-    lat: 34.67553087191428,
-    lng: 135.4933715822677,
-    title: "西大橋駅",
+    lat: 34.8440405,
+    lng: 135.4292319,
+    title: "p2",
     el: "treasure2",
     checked: false,
   },
+  {
+    id: 3,
+    lat: 34.8442985,
+    lng: 135.4291943,
+    title: "p3",
+    el: "treasure3",
+    checked: false,
+  },
 ];
+
+// export const data: TreasureData[] = [
+//   {
+//     id: 1,
+//     lat: 34.6745469127447,
+//     lng: 135.49032039571802,
+//     title: "堀江小学校",
+//     el: "treasure1",
+//     checked: false,
+//   },
+//   {
+//     id: 2,
+//     lat: 34.67553087191428,
+//     lng: 135.4933715822677,
+//     title: "西大橋駅",
+//     el: "treasure2",
+//     checked: false,
+//   },
+// ];
 const KEY = "savedData";
 export const saveState = (data: TreasureData[]) => {
   localStorage.setItem(KEY, JSON.stringify(data));
